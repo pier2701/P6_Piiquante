f638f846886fdcbae2ef6a689998e6f466d5d57b
pier2701 <pier2701@gmail.com>
2022-08-13 08:54:14 +0200

    signupCheck

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,24 +1,32 @@
-// on récupère la package "bcrypt" qu'on aura installer avec"npm
+// on récupère la package "bcrypt" pour hâcher le password
 const bcrypt = require("bcrypt");
+
+// on récupère la package "crypto-js" pour hâcher le "email"
+const cryptoJs = require("crypto-js");
 
 // on récupère la package "jsonwebtoken" à travers la constante
 const jwt = require("jsonwebtoken");
 
 const dotenv = require("dotenv");
 dotenv.config();
+const EMAILKEY = process.env.EMAILKEY;
 
 // on récupère notre model de "user"
 const User = require("../models/User");
 
 // on déclare une méthode ( middleware ) de signup pour enregistrer un nouvel "user"
 exports.signup = (req, res, next) => {
+  // on chiffre l'email
+  const emailCrypto = cryptoJs.HmacSHA256(req.body.email, EMAILKEY).toString();
+  console.log("email-crypté = " + emailCrypto);
+
   // on appelle la function de hash de "bcrypt" pour passer le mot de passe du corps du front pendant 10 tours
   bcrypt
     .hash(req.body.password, 10)
     .then((hash) => {
       // on récupère le "hash" du MDP qu'on va enregistrer dans un nouveau "user"
       const user = new User({
-        email: req.body.email,
+        email: emailCrypto,
         password: hash,
       });
       user
@@ -33,8 +41,9 @@ exports.signup = (req, res, next) => {
 // on déclare une méthode ( middleware ) de login pour que le "user" puisse se connecter
 // on met en place la logique pour trouver l'user puis on compare son mot de passe "hashé"
 exports.login = (req, res, next) => {
+  const emailCrypto = cryptoJs.HmacSHA256(req.body.email, EMAILKEY).toString();
   // recherche par le "champ" email et sa "valeur" dans le corps
-  User.findOne({ email: req.body.email })
+  User.findOne({ email: emailCrypto })
     // gestion d'un recherche réussie
     .then((user) => {
       if (!user) {
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,12 +8,15 @@ const router = express.Router();
 const userCtrl = require("../controllers/user");
 const { route } = require("./sauce");
 
+// on déclare le middleware de contrôle de l'email
+const regexEmail = require("../middleware/regexEmail");
+
 // on déclare le middleware de contrôle du mot de passe
 const pwdValidate = require("../middleware/password");
 
 // on récupére les 2 méthodes/functions/middlewares du "controlllers/user"
 // le frontend "post" les données, donc on utilise la méthode POST
-router.post("/signup", pwdValidate, userCtrl.signup); // "/signup" sera la route attndue par le frontend
+router.post("/signup", regexEmail, pwdValidate, userCtrl.signup); // "/signup" sera la route attndue par le frontend
 router.post("/login", userCtrl.login); // "/login" sera la route attndue par le frontend
 
 // on exporte ce routeur spécifique pour les "users"
